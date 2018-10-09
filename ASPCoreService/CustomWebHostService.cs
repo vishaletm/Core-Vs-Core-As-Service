d24264ad0ef4c6643b727306731b45959be7f382
vishaletm <vishaletm@gmail.com>
2018-10-10 00:23:42 +0530

    project added

---
+++ ASPCoreService/CustomWebHostService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreService
+{
+    internal class CustomWebHostService : WebHostService
+    {
+        public CustomWebHostService(IWebHost host) : base(host)
+        {
+        }
+
+        protected override void OnStarting(string[] args)
+        {
+            base.OnStarting(args);
+        }
+
+        protected override void OnStarted()
+        {
+            base.OnStarted();
+        }
+
+        protected override void OnStopping()
+        {
+            base.OnStopping();
+        }
+    }
+}
---
+++ ASPCoreService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPCoreService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
